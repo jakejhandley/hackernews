eec304055b9ba2f02edd1f9b1001a31028e01fa8
Jake Handley <jakejhandley@gmail.com>
2020-05-06 09:11:47 +0100

    Add files via upload

--- hacker.js
+++ hacker.js
@@ -6,28 +6,47 @@ const url_topstories = "http://hacker-news.firebaseio.com/v0/topstories.json";
 
 
 $.ajax({
-	url: url_maxitem,
-	success: function (story_maxitem){
-		console.log(story_maxitem)
+    url: url_jobstories,
+    success: function (story_jobstories) {
+        var liststories = $("#jobstories");
+        story_jobstories.slice(-10).forEach(function (id) {
+            $.ajax({
+                url: `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`,
+            success: function (jobstory) {
+                var entry = document.createElement('li');
+                var clickableName = document.createElement('a')
+                clickableName.href = jobstory["url"]
+                clickableName.target = "_blank"
+                var storyName = document.createTextNode(jobstory["title"])
+                clickableName.appendChild(storyName)
+                entry.appendChild(clickableName);
+                liststories.append(entry);
+            }
+        })
+        })
+    }}
+);
 
-		$("#maxitem").text(story_maxitem);
-	}
-})
+
 
 $.ajax({
-	url: url_jobstories,
-	success: function (story_jobstories){
-		console.log(story_jobstories)
-
-		$("#jobstories").text(story_jobstories);
-	}
-})
-
-$.ajax({
-	url: url_topstories,
-	success: function (story_topstories){
-		console.log(story_topstories)
-
-		$("#topstories").text(story_topstories);
-	}
-})
+    url: url_topstories,
+    success: function (story_topstories) {
+        var liststories = $("#topstories");
+        story_topstories.slice(-10).forEach(function (id) {
+            $.ajax({
+                url: `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`,
+            success: function (topstory) {
+                var entry = document.createElement('li');
+                var clickableName = document.createElement('a')
+                clickableName.href = topstory["url"]
+                clickableName.target = "_blank"
+                var storyName = document.createTextNode(topstory["title"])
+                clickableName.appendChild(storyName)
+                entry.appendChild(clickableName);
+                liststories.append(entry);
+            }
+        })
+        })
+    }}
+);
