b3bc604b0b5a216ecc93c32438669f61fd94a9db
Jake Handley <jakejhandley@gmail.com>
2020-05-06 07:23:34 +0100

    Add files via upload

--- hacker.js
+++ hacker.js
@@ -1,15 +1,33 @@
 "use strict"
 
-//hacker-news.firebaseio.com/v0/jobstories.json
-//hacker-news.firebaseio.com/v0/topstories.json
-
 const url_maxitem = "http://hacker-news.firebaseio.com/v0/maxitem.json";
+const url_jobstories = "http://hacker-news.firebaseio.com/v0/jobstories.json";
+const url_topstories = "http://hacker-news.firebaseio.com/v0/topstories.json";
+
 
 $.ajax({
 	url: url_maxitem,
-	success: function (story){
-		console.log(story)
+	success: function (story_maxitem){
+		console.log(story_maxitem)
 
-		$("#latestStory").text(story);
+		$("#maxitem").text(story_maxitem);
+	}
+})
+
+$.ajax({
+	url: url_jobstories,
+	success: function (story_jobstories){
+		console.log(story_jobstories)
+
+		$("#jobstories").text(story_jobstories);
+	}
+})
+
+$.ajax({
+	url: url_topstories,
+	success: function (story_topstories){
+		console.log(story_topstories)
+
+		$("#topstories").text(story_topstories);
 	}
 })
